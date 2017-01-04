8a0149ac6f90e5db68dc74da84c54635cec8bb46
Haley Swain and Skye Atwood <skye@>
2017-01-04 15:05:40 -0800

    successfully print bike title names when searching cities

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,14 +1,22 @@
 var apiKey = require('./../.env').apiKey;
+var bikes = []
+
+Bike = function(title, year) {
+  this.title = title;
+  this.year = year;
+}
 
 $(document).ready(function() {
   $('#bikeLocation').submit(function(event) {
     event.preventDefault();
-    debugger;
     var city = $('#location').val();
     $('#location').val("");
-    $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=100&location=' + city + '&distance=5&stolenness=stolen&appid=' + apiKey).then(function(response) {
-      console.log(response.bikes)
-      $('.showBikes').text("Lost Bikes in " + city + response.bikes.length);
+    $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=' + city + '&distance=5&stolenness=proximity&appid=' + apiKey).then(function(response) {
+      var bikes = response.bikes;
+      for (i = 0; i <= bikes.length; i++) {
+        var title = bikes[i].title;
+        $('#showBikes').append("<li>" + title + "</li>");
+      }
     });
   });
 });
