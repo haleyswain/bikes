6c679f6cf0aec5806f9d5b80e5caf71d1d69a064
Haley Swain and Skye Atwood <haley@>
2017-01-05 13:19:07 -0800

    add bike images

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -13,11 +13,12 @@ $(document).ready(function() {
     $('#location').val("");
     $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=' + city + '&distance=5&stolenness=proximity&appid=' + apiKey).then(function(response) {
       var bikes = response.bikes;
+
       for (i = 0; i <= bikes.length; i++) {
         var title = bikes[i].title;
         var colors = bikes[i].frame_colors;
-        console.log(colors);
-        $('#showBikes').append("<li>" + title + "<br>" + colors + "</li>");
+        var image = bikes[i].large_img;
+        $('#showBikes').append("<li>" + title + "<br>" + colors + '<br>' + "<img src='" + image +"'>"  + "</li>");
       }
       });
     });
